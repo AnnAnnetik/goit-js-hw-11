f6c741205b0890190bb749981d90f75ce894feed
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-02-01 14:10:56 +0200

    1

--- src/main.js
+++ src/main.js
@@ -5,3 +5,21 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import css from 'file.css';
+
+const refs = {
+    formEl: document.querySelector('.form'),
+    galleryEl: document.querySelector('.gallery'),
+};
+
+refs.formEl.addEventListener('submit', onSubmstForm);
+
+function onSubmstForm(e){
+    e.preventDefault();
+    const textSearch = e.target.elements.search.value;
+
+
+
+
+
+    e.target.reset();
+}
