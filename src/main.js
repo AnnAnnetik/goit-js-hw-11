80134702fb033864d0a3f6ba12efa4abf3200bcf
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-02-02 12:14:37 +0200

    1

--- src/main.js
+++ src/main.js
@@ -7,19 +7,68 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import css from 'file.css';
 
 const refs = {
-    formEl: document.querySelector('.form'),
-    galleryEl: document.querySelector('.gallery'),
+  formEl: document.querySelector('.form'),
+  galleryEl: document.querySelector('.gallery'),
 };
+const searchParams = new URLSearchParams({
+  key: '42110229-d56f9063956695e15527c98fc',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+});
 
-refs.formEl.addEventListener('submit', onSubmstForm);
+refs.formEl.addEventListener('submit', onSubmitForm);
 
-function onSubmstForm(e){
-    e.preventDefault();
-    const textSearch = e.target.elements.search.value;
+function onSubmitForm(e) {
+  e.preventDefault();
+  const textSearch = e.target.elements.search.value;
+  searchParams.q = textSearch;
+  fetchImg()
+    .then(images => renderImg(images))
+    .catch(error => console.log(error));
+  e.target.reset();
+}
 
+function fetchImg() {
+  return fetch('https://pixabay.com/api/?${searchParams}').then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
 
+function renderImg(images) {
+  if (images.length === 0) {
+    iziToast.show({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      messageColor: '#FAFAFB',
+      messageSize: '16px',
+      messageLineHeight: '24px',
+      backgroundColor: '#B51B1B',
+      color: '#FAFAFB',
+      position: 'topRight',
+    });
+  } else {
+    const galleryMarkup = images
+      .map(
+        image =>
+          `<li class="gallery-item">
+      <a class="gallery-link" href="${image.largeImageURL}">
+        <img
+          class="gallery-image"
+          src="${image.webformatURL}"
+              alt="${image.tags}"
+        />
+      </a>
+      </li>`
+      )
+      .join('');
 
+    refs.galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
-
-    e.target.reset();
+    let lightbox = new SimpleLightbox('.gallery a').refresh();
+  }
 }
